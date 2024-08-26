738d9b7a480b67d2719e178497ae07b87de525bf
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-26 11:40:09 +0300

    Update index.js and styles.css

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-    $('h1').css('color', 'red');
+$("h1").addClass("big-title")
+$(document).keypress(function(event) {
+    $("h1").text(String.fromCharCode(event.which));
 });
