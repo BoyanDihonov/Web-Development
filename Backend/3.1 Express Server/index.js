1f0b17871361ad299e042583a1b825ef612f6e1b
Boyan Dihonov <boyandihonov@abv.bg>
2024-08-30 15:54:11 +0300

    Update index.js

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -9,3 +9,13 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
     res.send("<h1>Helloooooo</h1>");
 });
+app.get("/register", (req, res) => {
+    res.send("<h1>Register form</h1>");
+});app.get("/about", (req, res) => {
+    res.send("<h1>My address will go here...</h1>");
+});app.get("/contacts", (req, res) => {
+    res.send("<h1>And my phone and email will go here :)</h1>");
+});
+app.get("*", (req, res) => {
+    res.send("<h1>404 ERROR    ! You mistyped something ! </h1>");
+});
