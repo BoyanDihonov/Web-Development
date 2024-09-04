6f9dd1efed21f8f25c5d5b55912f4c6994d4d726
Boyan Dihonov <boyandihonov@abv.bg>
2024-09-04 15:58:47 +0300

    Update index4.js

--- Backend/3.4 Middleware/index4.js
+++ Backend/3.4 Middleware/index4.js
@@ -1,7 +1,26 @@
+import bodyParser from "body-parser";
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import morgan from "morgan";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  const { street, pet } = req.body;
+  console.log(req.body);
+  res.send(`Your Band Name is: ${street}${pet}🖖`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
