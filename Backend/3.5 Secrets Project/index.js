d0c0e4d295929bf1a230d11407687d7cd1d1ebe7
Boyan Dihonov <boyandihonov@abv.bg>
2024-09-06 18:56:07 +0300

    Update index.js

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,35 +1,45 @@
 import express from "express";
 import bodyParser from "body-parser";
+import session from "express-session";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express();
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var userIsAuthorised = false;
+app.use(session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: true,
+}));
 
 function passwordCheck(req, res, next) {
     const password = req.body["password"];
     if (password === "ILoveProgramming") {
-        userIsAuthorised = true
+        req.session.userIsAuthorised = true;
     }
-    next()
+    next();
 }
 
 app.use(passwordCheck);
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
-})
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 app.post("/check", (req, res) => {
-    if (userIsAuthorised) {
-        res.sendFile(__dirname + "/public/secret.html")
+    if (req.session.userIsAuthorised) {
+        res.sendFile(path.join(__dirname, "/public/secret.html"));
     } else {
-        res.sendFile(__dirname + "/public/index.html")
+        res.sendFile(path.join(__dirname, "/public/index.html"));
     }
-})
+});
