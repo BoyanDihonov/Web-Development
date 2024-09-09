52303541708da6f1c9714ebd5d3db9236ebc5125
Boyan Dihonov <boyandihonov@abv.bg>
2024-09-09 15:38:05 +0300

    Update index.js

--- Backend/4.0 EJS/index.js
+++ Backend/4.0 EJS/index.js
@@ -3,10 +3,20 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res) =>{
-    res.render("index.ejs", {dayType: "a weekday", advice: "it's time to work hard"})
+app.get("/", (req, res) => {
+    const today = new Date()
+    const day = today.getDay()
+
+    let type = "a weekday"
+    let adv = "it's time to work hard"
+
+    if (day === 0 || day === 6) {
+        let type = "the weekend"
+        let adv = "it's time to have some fun"
+    }
+    res.render("index.ejs", { dayType: type, advice: adv })
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Server running on port ${port}`)
 })
